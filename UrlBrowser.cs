52cfa9ae888c2e4c4ebdadcce4eb1c9a03617a37
Pelle Tunell <pirkasmukko@msn.com>
2013-04-23 17:55:31 +0200

    la till felhantering av urladresser

--- UrlBrowser.cs
+++ UrlBrowser.cs
@@ -12,38 +12,38 @@ namespace Browser
     {
         public string requestHomepage(string url)
         {
-            // used to build entire input
+            try
+            {
             StringBuilder sb = new StringBuilder();
-            // used on each read operation
             byte[] buf = new byte[8192];
-            // prepare the web page we will be asking for
             HttpWebRequest request = (HttpWebRequest)
                 WebRequest.Create(url);
-            // execute the request
             HttpWebResponse response = (HttpWebResponse)
             request.GetResponse();
-            // we will read data via the response stream
             Stream resStream = response.GetResponseStream();
             string tempString = null;
             int count = 0;
 
             do
             {
-                // fill the buffer with data
                 count = resStream.Read(buf, 0, buf.Length);
-                // make sure we read some data
                 if (count != 0)
                 {
-                    // translate from bytes to ASCII text
                     tempString = Encoding.ASCII.GetString(buf, 0, count);
-                    // continue building the string
                     sb.Append(tempString);
                 }
             }
             while (count > 0); // any more data to read?
-            // print out page source
-            Console.WriteLine(sb.ToString());
             return sb.ToString();
+            }
+            catch(System.Net.WebException)
+            {
+                return "The remote name could not be resolved";
+            }
+            catch (System.UriFormatException)
+            {
+                return "Invalid URI: The format of the URI could not be determined";
+            }
         }
     }
 }
